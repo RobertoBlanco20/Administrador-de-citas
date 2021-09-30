5fc8e214776b42ecda04a5a48f0fabd2e01decc0
Roberto Blanco <86691875+RobertoBlanco20@users.noreply.github.com>
2021-09-30 08:43:38 -0300

    creando formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Formulario = () => {
     return ( 
-        <h1>Desde formulario</h1>
+        <Fragment>
+            <h2>Crear Cita</h2>
+
+            <form action="#" method="post">
+                <label for="mascota">Nombre Mascota</label>
+                <input 
+                    type="text" 
+                    name="mascota"
+                    className='u-full-width' 
+                    id="nombreMascota" 
+                    placeholder='Nombre Mascota' 
+                    required 
+                />
+                
+                <label for='nombreDuenio'>Nombre Dueño</label>
+                <input 
+                    type="text" 
+                    name='propietario' 
+                    className='u-full-width' 
+                    id="nombreDuenio" 
+                    placeholder='Nombre dueño de la mascota' 
+                    required 
+                />
+                
+                <label for='fecha'>Fecha</label>
+                <input 
+                    type='date' 
+                    name='fecha' 
+                    className='u-full-width' 
+                    id='fecha' 
+                    required 
+                />
+
+                <label for='hora'>Hora</label>
+                <input 
+                    type='time' 
+                    name='hora' 
+                    className='u-full-width' 
+                    id='hora'  
+                    required 
+                    />
+
+                <label for='sintomas'>Sintomas</label>
+                <textarea 
+                    name='sintomas' 
+                    id='sintomas' 
+                    className='u-full-width' 
+                ></textarea>
+
+                <br/>
+                <button 
+                    type="submit"
+                    className='u-full-width button-primary'  
+                    id='boton'
+                    >Agendar Cita
+                </button>
+            </form>
+
+        </Fragment>
      );
 }
  
